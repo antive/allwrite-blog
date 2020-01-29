b5545e6e0fa52a85886a7c2433543d05ca7b50f5
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2020-01-29 22:09:15 +0100

    :100:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Anders Mæhlum Halvorsen</code>
+          <code>Anders Mæhlum</code>
         </p>
         <a
           className="App-link"
