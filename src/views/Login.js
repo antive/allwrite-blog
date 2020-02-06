f55c23a45119cff1789875bac85207db2dcdf2a8
Sebastian Berge <sebastian@berge.im>
2020-02-07 00:24:27 +0100

    :boom:

--- src/views/Login.js
+++ src/views/Login.js
@@ -33,46 +33,48 @@ class Login extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<Container style={{ paddingTop: "70px" }}>
-					<h1 className='display-4'>Innlogging for admin/gruppemedlemmer</h1>
-					<Form onSubmit={this.handleSubmit}>
-						<Row form>
-							<Col md={6}>
-								<FormGroup>
-									<Label for='exampleEmail'>Epost</Label>
-									<Input
-										type='email'
-										name='email'
-										id='exampleEmail'
-										placeholder='Epost'
-									/>
-								</FormGroup>
-							</Col>
-							<Col md={6}>
-								<FormGroup>
-									<Label for='examplePassword'>Passord</Label>
-									<Input
-										type='password'
-										name='password'
-										id='examplePassword'
-										placeholder='Passord'
-									/>
-								</FormGroup>
-							</Col>
-							<Col>
-								<FormGroup>
-									<FormText color='muted'>
-										Har du problemer med innlogging? Vennligst kontakt
-										administrator for hjelp/brukerstøtte.
-									</FormText>
-								</FormGroup>
-							</Col>
-						</Row>
-						<Button color='danger'>Logg inn</Button>
-					</Form>
-				</Container>
-			</div>
+			<Container style={{ paddingTop: "70px" }}>
+				<h1 className='display-4 text-primary'>Logg inn på |The Blog|</h1>
+				<Row xs='2'>
+					<Col>
+						<Form onSubmit={this.handleSubmit}>
+							<Row form>
+								<Col md={6}>
+									<FormGroup>
+										<Label for='exampleEmail'>Epost</Label>
+										<Input
+											type='email'
+											name='email'
+											id='exampleEmail'
+											placeholder='Epost'
+										/>
+									</FormGroup>
+								</Col>
+								<Col md={6}>
+									<FormGroup>
+										<Label for='examplePassword'>Passord</Label>
+										<Input
+											type='password'
+											name='password'
+											id='examplePassword'
+											placeholder='Passord'
+										/>
+									</FormGroup>
+								</Col>
+								<Col>
+									<FormGroup>
+										<FormText color='muted'>
+											Har du problemer med innlogging? Vennligst kontakt
+											administrator for hjelp/brukerstøtte.
+										</FormText>
+									</FormGroup>
+								</Col>
+							</Row>
+							<Button color='danger'>Logg inn</Button>
+						</Form>
+					</Col>
+				</Row>
+			</Container>
 		)
 	}
 }
